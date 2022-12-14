cae6b28fb3fa06a6d726683d571b63396b123977
klop2211 <klop2211@gmail.com>
2022-12-14 16:17:08 +0900

    project update

---
+++ Foundation16~/file.h
@@ -0,0 +1,29 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+
+char* filetobuf(const char* file)
+{
+	FILE* fp;
+	long length;
+	char* buf;
+
+	fp = fopen(file, "rb");
+
+	if (!fp)
+		assert(fp != nullptr);
+
+	fseek(fp, 0, SEEK_END);
+	length = ftell(fp);
+	buf = (char*)malloc(sizeof(char) * length + 1);
+	fseek(fp, 0, SEEK_SET);
+	if (buf != nullptr)
+		fread(buf, 1, length + 1, fp);
+
+	fclose(fp);
+	if (buf != nullptr)
+		buf[length] = 0;
+
+	return buf;
+}
